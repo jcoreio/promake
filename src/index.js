4cc4d6aba9ec8476284e3d03e8229c9f3a8eb4c0
Andy Edwards <jedwards@fastmail.com>
2017-11-25 21:04:52 -0600

    fix(src): add missing exports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 // @flow
 
 import Promake from './Promake'
+import Rule from './Rule'
+import FileResource from './FileResource'
+import TaskResource from './TaskResource'
+import type {Resource} from './Resource'
 
 export default Promake
+export {Rule, FileResource, TaskResource}
+export type {Resource}
 
